2a122d329ccd336e0dee3863773cdf423c0c8be4
Asbjorn Kjaer <akjaer@riotgames.com>
2017-11-28 01:07:51 -0800

    Updating License field

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setuptools.setup(
         'Intended Audience :: Information Technology',
 
         # License information
-        'License :: OSI Approved :: Apache 2.0',
+        'License :: OSI Approved :: Apache Software License',
 
         # Supported python versions
         'Programming Language :: Python :: 3.5',
