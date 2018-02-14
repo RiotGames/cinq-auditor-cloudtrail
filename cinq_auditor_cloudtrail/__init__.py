15a2832d4892d9f7015840d266cbec00e6f92d2a
Asbjorn Kjaer <akjaer@riotgames.com>
2018-02-14 14:39:50 -0800

    Renamed db module to database

--- cinq_auditor_cloudtrail/__init__.py
+++ cinq_auditor_cloudtrail/__init__.py
@@ -1,9 +1,10 @@
 import json
 
 from botocore.exceptions import ClientError
-from cloud_inquisitor import get_aws_session, AWS_REGIONS, db
+from cloud_inquisitor import get_aws_session, AWS_REGIONS
 from cloud_inquisitor.config import dbconfig, ConfigOption
 from cloud_inquisitor.constants import NS_AUDITOR_CLOUDTRAIL, AccountTypes
+from cloud_inquisitor.database import db
 from cloud_inquisitor.plugins import BaseAuditor
 from cloud_inquisitor.schema import Account, AuditLog
 from cloud_inquisitor.utils import get_template
